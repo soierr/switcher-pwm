a126f44ba667589f57a72037cb0375f9f0d2b2ff
soierr <blackfin@ukr.net>
2019-01-13 00:22:35 +0200

    configure gitignore, initial source added

---
+++ lib/includes/libfahw.h
@@ -0,0 +1,9 @@
+#ifndef __FRIENDLYARM_HARDWARE_H__
+#define __FRIENDLYARM_HARDWARE_H__
+
+#include "../common.h"
+#include "libfahw-filectl.h"
+#include "libfahw-gpio.h"
+#include "libfahw-pwm.h"
+
+#endif
